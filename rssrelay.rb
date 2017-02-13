440e543d703706da77e48a88c9e75a3dc1c3fa13
Jamie Smith <i@jamiesmiths.com>
2017-02-13 07:10:44 +0000

    whole enclosure verified. turning on cache

--- rssrelay.rb
+++ rssrelay.rb
@@ -48,8 +48,7 @@ class RSSRelay < Sinatra::Application
     end
 
     # If the cache can serve the requested number of items, serve them from cache
-    #if cached_items && number_of_items_cached >= number_of_items_requested
-    if 1 < 0
+    if cached_items && number_of_items_cached >= number_of_items_requested
       parsed_cached_items["from_proxy_cache"] = true
       parsed_cached_items["items"] = parsed_cached_items["items"][0, 
         number_of_items_requested]
